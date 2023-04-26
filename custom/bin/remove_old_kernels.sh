68f4b0da86e9b893a1b7ddc188ee9372cb6368b1
dachrisch <christian.daehn@gmail.com>
2023-04-26 14:27:25 +0200

    new version to remove kernels

--- custom/bin/remove_old_kernels.sh
+++ custom/bin/remove_old_kernels.sh
@@ -1,19 +1,14 @@
 #!/bin/zsh
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
 
-if [ -z "$1" ];then
-  dry_run="--dry-run"
-  echo "running $0 in dry mode"
-else
-  dry_run=""
-  echo "running $0 in action mode"
-fi
-
-# https://askubuntu.com/questions/2793/how-do-i-remove-old-kernel-versions-to-clean-up-the-boot-menu
-dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt -y purge $dry_run
-
-if [ -z "$1" ];then
-  if read -q "choice?Really perform purge? (y/N): "; then
-    echo
-    $SHELL $0 -y
-  fi
-fi
+echo "${GREEN}current kernel version: ${YELLOW}$(uname -r )${NOCOLOR}"
+extra_modules=$(dpkg --list | grep -E "linux-modules-extra-[0-9]+.*" | grep ii | awk '{print $2}' | sort -V | sed -n '/'$(uname -r | cut -f1,2 -d"-")'/q;p'|xargs)
+echo "${RED}removing old extra modules:${YELLOW}${extra_modules}${NOCOLOR}"
+kernel_modules=$(dpkg --list | grep -E "linux-modules-[0-9]+.*" | grep ii | awk '{print $2}' | sort -V | sed -n '/'$(uname -r | cut -f1,2 -d"-")'/q;p'|xargs)
+echo "${RED}removing old modules:${YELLOW} ${kernel_modules}${NOCOLOR}"
+sudo apt remove $extra_modules $kernel_modules
+echo "${RED}autoremoving old kernels${NOCOLOR}"
+sudo apt autoremove

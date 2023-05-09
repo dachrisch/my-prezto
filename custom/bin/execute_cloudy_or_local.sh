22b01fcb78a68c0f9149b115f34eca4f97c576c6
dachrisch <christian.daehn@gmail.com>
2023-05-09 11:25:37 +0200

    remove old dirs

--- custom/bin/execute_cloudy_or_local.sh
+++ custom/bin/execute_cloudy_or_local.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-
-SCRIPT_DIR=/home/daehnc/Documents/Dropbox/scripts/bash/
-export PATH=$PATH:$SCRIPT_DIR
 timeout=$1
 if [ -z "${timeout##*[!0-9]*}" ];then
 	timeout=5
 fi
 
-if is_cloudy_backup_online.sh $timeout;then 
+if is_cloudy_backup_online.sh $timeout;then
 	echo "we are online...executing remote"
 	$*
 else
--- custom/bin/is_cloudy_backup_online.sh
+++ custom/bin/is_cloudy_backup_online.sh
@@ -5,7 +5,7 @@ if [ -z "${timeout##*[!0-9]*}" ];then
 	timeout=5
 fi
 
-if $(dirname $0)/is_online.sh cloudy $timeout;then
+if is_online.sh cloudy $timeout;then
 	if rsync --password-file="$HOME/.ssh/backup.rsync" backup@cloudy::Backup/delly/ > /dev/null;then
 		exit 0
 	fi

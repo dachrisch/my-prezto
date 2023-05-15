8b528fa889a7114582b1d5de47ec09a5bbda0a94
dachrisch <christian.daehn@gmail.com>
2023-05-15 16:51:37 +0200

    check if files need removeal

--- custom/bin/backup_home.sh
+++ custom/bin/backup_home.sh
@@ -88,7 +88,7 @@ if [ "$1" = "-l" ];then
 	if [ ! -d $ENCRYPT_DIR ];then mkdir -p $ENCRYPT_DIR;fi
 	pushd $ENCRYPT_DIR > /dev/null
 	encrypt_ssh.sh $backup_file
-	if ls $ENCRYPT_DIR/${BACKUP_NAME}_*.tgz.enc 1>/dev/null 2>&1;then
+	if [ $($ENCRYPT_DIR/${BACKUP_NAME}_*.tgz.enc |tail -n +2|wc -l) -gt 1 ];then
 		ls -t $ENCRYPT_DIR/${BACKUP_NAME}_*.tgz.enc |tail -n +2 | xargs rm --
 		ls -t $ENCRYPT_DIR/${BACKUP_NAME}_*.tgz.key.enc |tail -n +2 | xargs rm --
 	fi

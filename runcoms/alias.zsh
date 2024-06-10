644ef09027d9f9fdcbe97a5978e8d2b014c9a88a
dachrisch <christian.daehn@gmail.com>
2024-06-10 17:34:13 +0200

    Add alias for updating all packages and services

--- runcoms/alias.zsh
+++ runcoms/alias.zsh
@@ -2,8 +2,9 @@ alias ll='ls -al'
 # shellcheck disable=SC2142
 alias latest_installed_kernel_version='dpkg --list |grep -E "linux-modules-[0-9]+.*"|grep ii| awk "{print \$2}" | sort -V | tail -1 | cut -f3-5 -d"-"'
 alias snap-refresh='sudo snap refresh snap-store && sudo snap refresh'
-# linux-modules-extra need for storagebox utf-8 support
+# linux-modules-extra needed for storagebox utf-8 support
 alias apt-upgrade='sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt install linux-modules-extra-$(latest_installed_kernel_version)'
+alias update-all='apt-upgrade && snap-refresh && flatpak update'
 # helper for system services
 alias sys='systemctl --user'
 # helper for docker updates

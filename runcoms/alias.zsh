50eb8729f840d522856cba923262b6187f3ac61e
dachrisch <christian.daehn@gmail.com>
2026-01-22 17:09:47 +0100

    chore: remove extra module

--- runcoms/alias.zsh
+++ runcoms/alias.zsh
@@ -3,7 +3,7 @@ alias ll='ls -al'
 alias latest_installed_kernel_version='dpkg --list |grep -E "linux-modules-[0-9]+.*"|grep ii| awk "{print \$2}" | sort -V | tail -1 | cut -f3-5 -d"-"'
 alias snap-refresh='sudo snap refresh snap-store && sudo snap refresh'
 # linux-modules-extra needed for storagebox utf-8 support
-alias apt-upgrade='sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt install linux-modules-extra-$(latest_installed_kernel_version)'
+alias apt-upgrade='sudo apt update && sudo apt upgrade && sudo apt autoremove
 alias au=apt-upgrade
 alias update-all='apt-upgrade && snap-refresh && flatpak update'
 alias ua=update-all
